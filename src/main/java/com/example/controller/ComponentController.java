fd34dd28f73866888b7e7505ede514f7ca95a2bd
sepi0 <adrianngaspar@gmail.com>
2020-10-16 16:45:20 +0200

    Fixed typo in CORS update

--- src/main/java/com/example/controller/ComponentController.java
+++ src/main/java/com/example/controller/ComponentController.java
@@ -11,25 +11,25 @@ public class ComponentController {
     @Autowired
     private ComponentService componentService;
 
-    @CrossOrigin(origins = "https://sepi0.github.io/frag-builder-frontend/")
+    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/all")
     public @ResponseBody Iterable<Component> getAll() {
         return componentService.findAll();
     }
 
-    @CrossOrigin(origins = "https://sepi0.github.io/frag-builder-frontend/")
+    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/{id}")
     public @ResponseBody Component getById(@PathVariable Integer id) {
         return componentService.get(id);
     }
 
-    @CrossOrigin(origins = "https://sepi0.github.io/frag-builder-frontend/")
+    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/{type}/{model}")
     public @ResponseBody Iterable<Component> getByTypeAndModel(@PathVariable String type, @PathVariable String model) {
         return componentService.findByTypeAndModel(type, model);
     }
 
-    @CrossOrigin(origins = "https://sepi0.github.io/frag-builder-frontend/")
+    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/{type}/all")
     public @ResponseBody Iterable<Component> getAllByType(@PathVariable String type) {
         return componentService.findByType(type);
