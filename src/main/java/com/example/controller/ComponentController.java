d65fa4482a0896b7e4281aebfa4653cd4c662407
sepi0 <adrianngaspar@gmail.com>
2020-10-16 19:08:48 +0200

    CORS test

--- src/main/java/com/example/controller/ComponentController.java
+++ src/main/java/com/example/controller/ComponentController.java
@@ -6,30 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/api")
 public class ComponentController {
     @Autowired
     private ComponentService componentService;
 
-    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/all")
     public @ResponseBody Iterable<Component> getAll() {
         return componentService.findAll();
     }
 
-    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/{id}")
     public @ResponseBody Component getById(@PathVariable Integer id) {
         return componentService.get(id);
     }
 
-    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/{type}/{model}")
     public @ResponseBody Iterable<Component> getByTypeAndModel(@PathVariable String type, @PathVariable String model) {
         return componentService.findByTypeAndModel(type, model);
     }
 
-    @CrossOrigin(origins = "https://sepi0.github.io/")
     @GetMapping(path = "/components/{type}/all")
     public @ResponseBody Iterable<Component> getAllByType(@PathVariable String type) {
         return componentService.findByType(type);
--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/order")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
-    @CrossOrigin(origins = "https://sepi0.github.io/frag-builder-frontend/")
     @PostMapping(path = "/send")
     public Order newOrder(@RequestBody Order order) {
         orderService.save(order);
